1f60e2ed6f9d7e3996415ec412bd1c4e9162a62a
Serg Malkof <sergmalkof@gmail.com>
2019-04-22 23:23:50 +0300

    no magic anymore

--- testscript.sh
+++ testscript.sh
@@ -2,6 +2,4 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "yeah, science"
-echo "23:01"
-echo "feel the power of the magic" 
+echo "yeah, science, science!"
